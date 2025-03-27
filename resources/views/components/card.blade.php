c1e259d6c02bd58474977ecb3924e3ca5ce7db0c
birukashagre6 <mandave693@gmail.com>
2025-03-27 11:26:12 +0300

    creating components

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,3 @@
+<div class="card">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde non enim veniam nostrum. Minima debitis sit dignissimos explicabo repellendus aliquam delectus vitae ipsum excepturi modi! Nobis neque laudantium autem error?
+</div>
