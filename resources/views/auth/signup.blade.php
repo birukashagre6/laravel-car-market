c01cfb2f8e703cc925c87f6bfe3875550f35166f
birukashagre6 <mandave693@gmail.com>
2025-03-31 18:32:47 +0300

    configure routing and layouts

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,52 +1,36 @@
+<x-guest-layout title="Signup Page" bodyClass="page-signup">
 
-<x-base-layout title="Signup Page" bodyClass="page-signup">
-  <main>
-    <div class="container-small page-login">
-      <div class="flex" style="gap: 5rem">
-        <div class="auth-page-form">
-          <div class="text-center">
-            <a href="/">
-              <img src="/img/logoipsum-265.svg" alt="" />
-            </a>
-          </div>
-          <h1 class="auth-page-title">Signup</h1>
+<h1 class="auth-page-title">Signup</h1>
 
-          <form action="" method="post">
-            <div class="form-group">
-              <input type="email" placeholder="Your Email" />
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Your Password" />
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Repeat Password" />
-            </div>
-            <hr />
-            <div class="form-group">
-              <input type="text" placeholder="First Name" />
-            </div>
-            <div class="form-group">
-              <input type="text" placeholder="Last Name" />
-            </div>
-            <div class="form-group">
-              <input type="text" placeholder="Phone" />
-            </div>
-            <button class="btn btn-primary btn-login w-full">Register</button>
+<form action="" method="post">
+  <div class="form-group">
+    <input type="email" placeholder="Your Email" />
+  </div>
+  <div class="form-group">
+    <input type="password" placeholder="Your Password" />
+  </div>
+  <div class="form-group">
+    <input type="password" placeholder="Repeat Password" />
+  </div>
+  <hr />
+  <div class="form-group">
+    <input type="text" placeholder="First Name" />
+  </div>
+  <div class="form-group">
+    <input type="text" placeholder="Last Name" />
+  </div>
+  <div class="form-group">
+    <input type="text" placeholder="Phone" />
+  </div>
+  <button class="btn btn-primary btn-login w-full">Register</button>
+  <x-slot:footerLink>
+  Already have an account? -
+  <a href="/login.html"> Click here to login </a>
+</x-slot:footerLink>
+</form>
 
-            <div class="grid grid-cols-2 gap-1 social-auth-buttons">
-             <x-google-button />
-             <x-fb-button />
-            </div>
-            <div class="login-text-dont-have-account">
-              Already have an account? -
-              <a href="/login.html"> Click here to login </a>
-            </div>
-          </form>
-        </div>
-        <div class="auth-page-image">
-          <img src="/img/car-png-39071.png" alt="" class="img-responsive" />
-        </div>
-      </div>
-    </div>
-  </main>
-</x-base-layout>
+
+</x-guest-layout>
+
+
+
